5ded56f2f882afe21c22b1c7d6eeca6c825c6ce8
Al-Hasan <alhasann352@gmail.com>
2026-03-03 00:48:19 +0600

    colorfull text

--- app/src/main/java/com/example/demoapplication/MainActivity.kt
+++ app/src/main/java/com/example/demoapplication/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.demoapplication.module1.SimpleText
+import com.example.demoapplication.module1.ColorFullText
 import com.example.demoapplication.ui.theme.DemoApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +15,8 @@ class MainActivity : ComponentActivity() {
             DemoApplicationTheme {
                 //Greeting()
                 //ResourceShow()
-                SimpleText()
+                //SimpleText()
+                ColorFullText()
             }
         }
     }
--- app/src/main/java/com/example/demoapplication/module1/TextTypoGraphy.kt
+++ app/src/main/java/com/example/demoapplication/module1/TextTypoGraphy.kt
@@ -6,12 +6,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 import com.example.demoapplication.R
 
@@ -28,5 +32,30 @@ fun SimpleText() {
                 shadow = Shadow(color = Color.Blue, blurRadius = 10f)
             )
         )
+    }
+}
+
+@Composable
+fun ColorFullText() {
+    val rainbowColor = listOf(
+        Color(0xFFFF0000),
+        Color(0xFFFF7F00),
+        Color(0xFFFFFF00),
+        Color(0xFF00FF00),
+        Color(0xFF0000FF),
+        Color(0xFF4B0082),
+        Color(0xFF8F00FF)
+    )
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(buildAnnotatedString {
+            append("Hello Jetpack Compose \n")
+            withStyle(
+                style = SpanStyle(brush = Brush.linearGradient(rainbowColor))
+            ) {
+                append("Native Android Developers")
+            }
+            append("\nI am learning Jetpack Compose")
+        })
     }
 }
