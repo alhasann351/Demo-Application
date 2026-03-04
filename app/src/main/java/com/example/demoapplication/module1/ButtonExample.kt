65f05d0ee1662e770c2f6e04673843fad95e5fa7
Al-Hasan <alhasann352@gmail.com>
2026-03-05 01:18:39 +0600

    outline elevated text button

--- app/src/main/java/com/example/demoapplication/module1/ButtonExample.kt
+++ app/src/main/java/com/example/demoapplication/module1/ButtonExample.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +30,21 @@ fun FilledButtonExample() {
                 Toast.makeText(context, "Tonal Button is clicked", Toast.LENGTH_SHORT).show()
             }) {
                 Text("Tonal Button")
+            }
+            OutlinedButton(onClick = {
+                Toast.makeText(context, "Outline Button clicked", Toast.LENGTH_SHORT).show()
+            }) {
+                Text("Outline Button")
+            }
+            ElevatedButton(onClick = {
+                Toast.makeText(context, "Elevated Button clicked", Toast.LENGTH_SHORT).show()
+            }) {
+                Text("Elevated Button")
+            }
+            TextButton(onClick = {
+                Toast.makeText(context, "Text Button clicked", Toast.LENGTH_SHORT).show()
+            }) {
+                Text("Text Button")
             }
         }
     }
